790b8de4b3a61d03880eecbf056958dcc15a885b
Marcelino Mejia <mmejia1@tecbaco.com>
2021-09-22 16:31:15 -0500

    Nuevo taller

---
+++ CalcularPorcentaje/porcentaje.js
@@ -0,0 +1,9 @@
+/* const preciOriginal = 120;
+const descuento = 18 ; */
+
+function calularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
