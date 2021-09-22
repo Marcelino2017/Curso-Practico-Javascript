f255b3b1cb9f54b1c0078aff4835c0b100270325
Marcelino Mejia <mmejia1@tecbaco.com>
2021-09-22 15:16:15 -0500

    Fin del taller

--- figuras.js
+++ figuras.js
@@ -54,6 +54,35 @@ function areaCirculo(radio) {
     return (radio * radio) * PI;
 }
 
-
-
 console.groupEnd();
+
+//Aquí interactuamos con el HTML
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    
+    const permitro = perimetroCuadrado(value);
+    alert ('El permitro del cuadrado es: ' + permitro + " cm")
+
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    
+    const area = areaCuadrado(value);
+    alert ('El área del cuadrado es: ' + area + " cm")
+}
+
+function alturaTrianguloIsosceles() {
+    const ladoA = document.getElementById("lado1T").value;
+    const ladoB = document.getElementById("lado2T").value;
+    const baseT = document.getElementById("baseT").value;
+
+    if(ladoA == ladoB && ladoA != baseT){
+       alturaT = Math.sqrt((ladoA**2 - (baseT**2 / 4)));
+       alert("La artura del triangulo isoceles es: " + alturaT + " cm");
+    } else {
+        alert("No es un triangulo isoceles");
+    }
+}
