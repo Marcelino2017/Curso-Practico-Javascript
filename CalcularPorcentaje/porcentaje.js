f6ca5e6afe1cc78944d8e66b3a8ead78c3d9920e
Marcelino Mejia <mmejia1@tecbaco.com>
2021-09-23 11:17:29 -0500

    Calcular el descuento

--- CalcularPorcentaje/porcentaje.js
+++ CalcularPorcentaje/porcentaje.js
@@ -1,9 +1,19 @@
-/* const preciOriginal = 120;
-const descuento = 18 ; */
+
 
 function calularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
     return precioConDescuento;
+}
+
+function onClickButtonPriceDisconect() {
+    const inputPrice =  document.getElementById('InputPrice').value;
+    const inputValue =  document.getElementById('InputDiscount').value;
+    const precioConDescuento = calularPrecioConDescuento(inputPrice, inputValue);
+
+    const resutado = document.getElementById("ResultP")
+    resutado.innerText = `El precio con descunto son de: $ ${precioConDescuento}`;
+    //ResultP
+
 }
